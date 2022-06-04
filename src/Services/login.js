e9190ba9b2ab383adc9110fb98b42173b1ef6d21
Diogo Ferreira <diogoff@live.com>
2022-06-04 19:43:14 +0200

    TokenId implementation, database error

--- src/Services/login.js
+++ src/Services/login.js
@@ -1,6 +1,7 @@
 import { GoogleLogin } from 'react-google-login';
 import ProductDataService from "../Services/Barcelparts.js"
 import React from "react";
+import http from "../http-common";
 
 const clientId = "1040605938120-vj3qmpjvouj820vrum6lu196p1j1p2jp.apps.googleusercontent.com";
 
@@ -9,8 +10,6 @@ function Login() {
     const verifyIfUserExists = (userData) => {
         ProductDataService.findUser(userData)
             .then(response => {
-                //Console log for debugging and developing
-                console.log(response.data)
                 if (response.data == null) {              //if the user doesn't exist, we create it
                     ProductDataService.createUser(userData)
                 }
@@ -24,21 +23,37 @@ function Login() {
 
     const onSuccess = (res) => {
 
-        var userData = {
-            "_id": res.profileObj.googleId,
-            "User_FirstName": res.profileObj.givenName,
-            "User_LastName": res.profileObj.familyName,
-            "User_Image": res.profileObj.imageUrl,
-            "Email": res.profileObj.email
-        }
 
-        verifyIfUserExists(userData);
-        console.log("LOGIN SUCCESS! CUrrent user: ", res.profileObj)
+        //we receive a token that we need to validate/decode to obtain the user info
+        console.log("received token = " + res.tokenId)
 
-        var CryptoJS = require("crypto-js");
-        //Encrypt THE COOKIE and add it to the browser
-        document.cookie = "userGoogleId=" + CryptoJS.AES.encrypt(res.profileObj.googleId, 'secret key 123').toString();
-        window.location.href = "/";
+        // TODO this should be done by the server I believe, not in the frontend
+        //send the token to google to be decoded
+        http.get(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=` + res.tokenId)
+            .then(response => {
+
+                console.log("here we go")
+                console.log(response.data)
+
+                //use the decoded token to get the user info
+                var userData = {
+                    "_id": response.data.googleId,
+                    "User_FirstName": response.data.given_name,
+                    "User_LastName": response.data.family_name,
+                    "User_Image": response.data.picture,
+                    "Email": response.data.email
+                }
+
+                verifyIfUserExists(userData);
+                console.log("LOGIN SUCCESS! CUrrent user: ", userData)
+
+                var CryptoJS = require("crypto-js");
+                //Encrypt THE COOKIE and add it to the browser
+                document.cookie = "userGoogleId=" + CryptoJS.AES.encrypt(userData.googleId, 'secret key 123').toString();
+                //window.location.href = "/";
+
+
+            })
     }
 
     const onFailure = (res) => {
@@ -55,6 +70,7 @@ function Login() {
                 onSuccess={onSuccess}
                 onFailure={onFailure}
                 cookiePolicy={'single_host_origin'}
+                responseType='id_token'
             />
         </div>
     )
